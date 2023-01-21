1e0c55f11078742cb5857b35daad6e5c4029b38c
HOP3N <pankoff97@gmail.com>
2023-01-21 14:43:31 +0200

    t1+?

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,46 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+const itemEl = [];
+
+galleryItems.forEach((element) => {
+  const galleryItem = document.createElement('div');
+  galleryItem.className = 'gallery__item';
+  const galleryLink = document.createElement('a');
+  galleryLink.className = 'gallery__link';
+  galleryLink.href = element.original;
+  const galleryImage = document.createElement('img');
+  galleryImage.className = 'gallery__image';
+  galleryImage.src = element.preview;
+  galleryImage.setAttribute('data-source', element.original);
+  galleryImage.alt = element.description;
+
+  galleryItem.append(galleryLink);
+  galleryLink.append(galleryImage);
+  itemEl.push(galleryItem);
+});
+
+galleryEl.append(...itemEl);
+
+galleryEl.addEventListener('click', (el) => {
+  el.preventDefault();
+  if (el.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const onSelectImg = el.target.getAttribute('data-source');
+
+  const elementEl = basicLightbox.create(
+    `<img src="${onSelectImg}" width="800" height="600"`
+  );
+
+  elementEl.show();
+
+  galleryEl.addEventListener('keydown', (el) => {
+    if (el.key === 'Escape') {
+      elementEl.close();
+    }
+  });
+});
